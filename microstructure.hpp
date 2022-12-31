208f98a86a00b5184d606ff574b95f4fecb9735f
M1TE5H <M1TE5H@oqtopi.com>
2022-12-31 17:42:37 +0100

    Create microstructure.hpp

---
+++ microstructure.hpp
@@ -0,0 +1,42 @@
+/**
+ *
+ * MICROSTRUCTURE
+ *
+ * @file:  microstructure.hpp 
+ * @brief: Simulation and Visualisation of Mesoscale Phenomena
+ *
+ * ================================================================
+ * USAGE
+ *
+ * #include "microstructure.hpp"
+ *
+ * int main () {
+ *     microstructure::simulation();
+ *     microstructure::visualisation();
+ * }
+ *
+ * ================================================================
+ */
+
+#ifndef __MICROSTRUCTURE_HPP__
+#define __MICROSTRUCTURE_HPP__
+
+#include <cmath>
+#include <cstddef>
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+/**
+ *
+ *
+ */
+
+namespace microstructure {
+
+} // namespace microstructure
+
+#endif // __MICROSTRUCTURE_HPP__
